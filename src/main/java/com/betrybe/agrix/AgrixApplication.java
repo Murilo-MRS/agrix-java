abb0ac1250fd43c8c04bf5f983fc4e01aa81d56a
Murilo-MRS <murilosantana1993@gmail.com>
2025-06-24 23:28:44 -0300

    fix: removendo linha

--- src/main/java/com/betrybe/agrix/AgrixApplication.java
+++ src/main/java/com/betrybe/agrix/AgrixApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class AgrixApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(AgrixApplication.class, args);
   }
